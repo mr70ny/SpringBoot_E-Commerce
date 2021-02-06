5852ccbcc35685c7f177558a672978089da0c125
User <user.push@hackerrank.workspace>
2021-02-06 22:41:18 +0000

    Auto save changes
    Commit by HackerRank

--- src/main/java/com/hackerrank/eshopping/product/dashboard/controller/ProductsController.java
+++ src/main/java/com/hackerrank/eshopping/product/dashboard/controller/ProductsController.java
@@ -1,10 +1,20 @@
 package com.hackerrank.eshopping.product.dashboard.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.hackerrank.eshopping.product.dashboard.model.Product;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/products")
 public class ProductsController {
+
+    @PostMapping(value = "/products", produces = "application/json",
+    consumes = "application/json")
+    public Product addNewProduct(@RequestBody Product product) {
+
+
+      
+    }
 
 }
